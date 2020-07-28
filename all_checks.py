cbda26c24415757ab5536b914d0351819a5dcce1
ahmedzaki217 <40589861+ahmedzaki217@users.noreply.github.com>
2020-07-28 14:12:59 +0200

    Add all_checks.py file to the remote rebo manually

---
+++ all_checks.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3.8
+import os
+import sys
+        
+def check_reboot():
+    """Returns true if vm has pending rebooted"""
+    return os.path.exists("/run/reboot-required")
+
+def main():
+    if check_reboot():
+        print("Pending Reboot.")
+        sys.exit(1)
+    print("Everything ok.")
+    sys.exit(0)
+main()
