991c294792f3aa7a8d53354e7da1515005aba479
Ahmed Zaki <ahmedmohamedzaki217@gmail.com>
2020-07-28 16:03:19 +0200

    change min_abs to min_gb, use parameters names

--- all_checks.py
+++ all_checks.py
@@ -7,7 +7,7 @@ def check_reboot():
     """Returns true if vm has pending rebooted"""
     return os.path.exists("/run/reboot-required")
 
-def check_disk_full(disk, min_abs, min_percent):
+def check_disk_full(disk, min_gb, min_percent):
     """Retruns true if there is not enaugh disk space other wise false"""
     du = shutil.disk_usage(disk)
     #calculate the percentage of free space
@@ -15,7 +15,7 @@ def check_disk_full(disk, min_abs, min_percent):
     #calculate how many gigabytes free
     gigabytes_free = du.free / 2**30
     
-    if percent_free < min_percent or gigabytes_free < min_abs:
+    if percent_free < min_percent or gigabytes_free < min_gb:
         return True
     return False
 
@@ -23,7 +23,7 @@ def main():
     if check_reboot():
         print("Pending Reboot.")
         sys.exit(1)
-    if check_disk_full("/", 2, 10):
+    if check_disk_full(disk="/", min_gb = 2, min_percent = 10):
         print("Disk is full.")
         sys.exit(1)
     print("Everything ok.")
