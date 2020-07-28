5b5772c2a4047942bb70a38cba2708e8c7dcf67c
ahmedzaki217 <40589861+ahmedzaki217@users.noreply.github.com>
2020-07-28 15:31:34 +0200

    add check_disk_full functions.

--- all_checks.py
+++ all_checks.py
@@ -1,14 +1,30 @@
 #!/usr/bin/env python3.8
 import os
 import sys
+import shutil
         
 def check_reboot():
     """Returns true if vm has pending rebooted"""
     return os.path.exists("/run/reboot-required")
 
+def check_disk_full(disk, min_abs, min_percent):
+    """Retruns true if there is not enaugh disk space other wise false"""
+    du = shutil.disk_usage(disk)
+    #calculate the percentage of free space
+    percent_free = 100 * du.free / du.total
+    #calculate how many gigabytes free
+    gigabytes_free = du.free / 2**30
+    
+    if percent_free < min_percent or gigabytes_free < min_abs:
+        return true
+    return false
+
 def main():
     if check_reboot():
         print("Pending Reboot.")
+        sys.exit(1)
+    if check_disk_full("/", 2, 10):
+        print("Disk is full.")
         sys.exit(1)
     print("Everything ok.")
     sys.exit(0)
